2187b7f41d7583af3cba02200e93160212d73f93
JUNEHYUNG\USER <cjh951114@gmail.com>
2023-10-04 16:43:42 +0900

    07. Vue.js 최종 프로젝트

---
+++ practice/vue3-form/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <form action="" @submit.prevent="submitForm">
+    <div>
+      <label for="userId">ID : </label>
+      <input id="userId" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">PW : </label>
+      <input id="password" type="password" v-model="password" />
+    </div>
+    <button type="submit">LOGIN</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: "App",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    sumbmitForm(){
+      // e.preventDefault();
+      // console.log('제출됨')
+      const data = {
+        username: this.username,
+        password: this.password
+      }
+      
+      axios.post('https://jsonplaceholder.typicode.com/users/', data)
+      .then((res)=>{
+        console.log(res);
+      });
+
+      // this.username;
+      // this.password;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ practice/vue3-form/src/App_setup.vue
@@ -0,0 +1,56 @@
+<template>
+  <form action="" @submit.prevent="submitForm">
+    <div>
+      <label for="userId">ID : </label>
+      <input id="userId" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">PW : </label>
+      <input id="password" type="password" v-model="password" />
+    </div>
+    <button type="submit">LOGIN</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+import { ref } from 'vue';
+export default {
+  name: "AppSetup",
+  setup(){
+    const username = ref('');
+    const password = ref('');
+
+    // methods
+    const submitForm=()=>{
+      // e.preventDefault();
+      // console.log('제출됨')
+      const data = {
+        username: username.value,
+        password: password.value
+      }
+      
+      axios.post('https://jsonplaceholder.typicode.com/users/', data)
+      .then((res)=>{
+        console.log(res);
+      });
+    }
+    return {
+      username,
+      password,
+      submitForm
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
