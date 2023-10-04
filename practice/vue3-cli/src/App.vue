28c30b0a5d76e3d4b01595807b2f1719d1774d51
JUNEHYUNG\USER <cjh951114@gmail.com>
2023-10-04 15:36:55 +0900

    06. Vue 싱글 파일 컴포넌트

--- practice/vue3-cli/src/App.vue
+++ practice/vue3-cli/src/App.vue
@@ -1,15 +1,30 @@
 <template>
+  <AppHeader :appTitle="message" @change="changeMessage"/>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+// import 라이브러리 from '라이브러리 이름'
+// import 파일이름 from '상대경로'
 import HelloWorld from './components/HelloWorld.vue'
-
+import AppHeader from './components/AppHeader.vue'
 export default {
+  // 인스턴스 옵션 속성
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    AppHeader
+  },
+  data(){
+    return {
+      message: 'App Header Component'
+    }
+  },
+  methods:{
+    changeMessage(){
+      this.message = '변경된 App Header'
+    }
   }
 }
 </script>
---
+++ practice/vue3-cli/src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ App Title }}</h1>
+  <button @click="changeTitle">변경</button>
+</template>
+
+<script>
+  export default {
+    props: ['appTitle'],
+    methods: {
+      changeTitle(){
+        this.$emit('change')
+      }
+    }
+  }
+</script>
+<style></style>
