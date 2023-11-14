7f4576f13b6cad818231550153463abb0d8f5792
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-11-14 17:05:08 +0900

    02. Vue Composition API 활용 : 뒷부분은 01 완성후 다시 수강.

---
+++ practice/Vue3-CompositionAPI/vue3-todo/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <TodoHeader :appTitle="title"/>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      title: '할일 앱'
+    }
+  }
+}
+</script>
---
+++ practice/Vue3-CompositionAPI/vue3-todo/src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li v-for="(item, idx) in todoItems" :key="idx">
+      <span>{{ item }}</span>
+      <buton>삭제</buton>
+    </li>
+  </ul>  
+</template>
+
+<script>
+  export default {
+    props: ['todoItems', 'userId'],
+    setup(props, context){
+      function removeTodo(item, index){
+        context.emit('remove', item, index);
+      }
+      return {removeTodo}
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
